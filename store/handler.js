0a089cfeb55d68fc5a5e40eec9fffa55a695e1d6
Daniel Ireson <danielireson@gmail.com>
2017-09-10 20:59:38 +0100

    setup store handler structure

--- store/handler.js
+++ store/handler.js
@@ -1,13 +1,52 @@
 'use strict'
 
-module.exports.handle = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'store function called',
-      input: event,
-    }),
-  }
+const url = require('url')
 
-  callback(null, response)
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3()
+
+const config = require('../config.json')
+
+module.exports.handle = (event, context, callback) => {
+  validateURL(event.url)
+    .then(function (url) {
+      return S3.putObject(buildRedirect(url)).promise()
+    })
+    .then(function (response) {
+      return Promise.resolve(buildResponse(200, 'URL successfully shortened'))
+    })
+    .catch(function (err) {
+      return Promise.resolve(buildResponse(err.statusCode, err.message))
+    })
+    .then(function (response) {
+      callback(null, response)
+    })
+}
+
+function buildRedirect (url) {
+  return {
+    Bucket: config.BUCKET,
+    Key: 'test'
+  }
+}
+
+function buildResponse (statusCode, message) {
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      message: message,
+    }) 
+  }
+}
+
+function validateURL (str = '') {
+  let urlObj = url.parse(str)
+  if (urlObj.protocol !== null && urlObj.host !== null) {
+    return Promise.resolve(str)
+  } else {
+    return Promise.reject({
+      statusCode: 400,
+      message: 'Long URL is invalid'
+    })
+  }
 }
