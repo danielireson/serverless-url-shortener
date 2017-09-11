0388206862a44c4c9b23fc4c41e4649b8b66091b
Daniel Ireson <danielireson@gmail.com>
2017-09-11 22:38:33 +0100

    add short url generate

--- store/handler.js
+++ store/handler.js
@@ -8,26 +8,66 @@ const S3 = new AWS.S3()
 const config = require('../config.json')
 
 module.exports.handle = (event, context, callback) => {
-  validateURL(event.url)
-    .then(function (url) {
-      return S3.putObject(buildRedirect(url)).promise()
+  validate(event.url)
+    .then(function () {
+      return getShortUrl()
     })
-    .then(function (response) {
-      return Promise.resolve(buildResponse(200, 'URL successfully shortened'))
+    .then(function (shortUrl) {
+      let redirect = buildRedirect(shortUrl, event.url)
+      return S3.putObject(redirect).promise()
+    })
+    .then(function () {
+      let response = buildResponse(200, 'URL successfully shortened')
+      return Promise.resolve(response)
     })
     .catch(function (err) {
-      return Promise.resolve(buildResponse(err.statusCode, err.message))
+      let response = buildResponse(err.statusCode, err.message)
+      return Promise.resolve(response)
     })
     .then(function (response) {
       callback(null, response)
     })
 }
 
-function buildRedirect (url) {
-  return {
-    Bucket: config.BUCKET,
-    Key: 'test'
+function getShortUrl () {
+  return new Promise(function (resolve, reject) {
+    let shortUrl = generateShortUrl()
+    isShortUrlFree(shortUrl)
+      .then(function (isFree) {
+        return isFree ? resolve(shortUrl) : resolve(getShortUrl())
+      })
+  })
+}
+
+function generateShortUrl (shortUrl = '') {
+  let characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  let position = Math.floor(Math.random() * characters.length)
+  let character = characters.charAt(position)
+
+  if (shortUrl.length === 7) {
+    return shortUrl
   }
+
+  return generateShortUrl(shortUrl + character)
+}
+
+function isShortUrlFree (shortUrl) {
+  return S3.headObject(buildRedirect(shortUrl)).promise()
+    .then(() => Promise.resolve(false))
+    .catch(() => Promise.resolve(true))
+}
+
+function buildRedirect (shortUrl, longUrl = false) {
+  let redirect = {
+    'Bucket': config.BUCKET,
+    'Key': shortUrl,
+  }
+
+  if (longUrl) {
+    redirect['WebsiteRedirectLocation'] = longUrl
+  }
+
+  return redirect
 }
 
 function buildResponse (statusCode, message) {
@@ -39,7 +79,7 @@ function buildResponse (statusCode, message) {
   }
 }
 
-function validateURL (str = '') {
+function validate (str = '') {
   let urlObj = url.parse(str)
   if (urlObj.protocol !== null && urlObj.host !== null) {
     return Promise.resolve(str)
