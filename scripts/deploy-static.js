b41f5ed0240d446eb86f24044544681387c9a708
Daniel Ireson <danielireson@gmail.com>
2017-09-14 21:08:43 +0100

    print url on deploy static script success

--- scripts/deploy-static.js
+++ scripts/deploy-static.js
@@ -9,9 +9,12 @@ const command = `aws s3 sync static s3://${config.BUCKET}`
 exec(command, function (error, stdout, stderr) {
   if (stderr) {
     console.error('Deploy static error')
+    console.error('---')
     console.error(stderr)
   } else {
     console.log('Deploy static success')
+    console.log(`https://${config.BUCKET}.s3.${config.REGION}.amazonaws.com/`)
+    console.log('---')
     console.log(stdout)
   }
 })
