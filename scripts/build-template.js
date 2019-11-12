94c927a26d1d68031b2e3e6fc78a0f68afdb9c2f
Joel Van Horn <joel@joelvanhorn.com>
2019-11-11 22:32:16 -0500

    Make UI customizable

--- scripts/build-template.js
+++ scripts/build-template.js
@@ -3,14 +3,24 @@
 const fs = require('fs')
 const path = require('path')
 
-const { API_URL } = process.env
+const { API_URL, TITLE, CREDIT, CREDIT_URL } = process.env
 
 try {
   const readPath = path.resolve(__dirname, '../src/template.html')
   const template = fs.readFileSync(readPath).toString()
 
   const writePath = path.resolve(__dirname, '../build/index.html')
-  fs.writeFileSync(writePath, template.replace('INSERT_API_URL', API_URL))
+  fs.writeFileSync(
+    writePath,
+    template
+      .replace(/INSERT_API_URL/g, API_URL)
+      .replace(/INSERT_TITLE/g, TITLE || 'serverless url shortener')
+      .replace(
+        /INSERT_CREDIT_URL/g,
+        CREDIT_URL || 'https://github.com/danielireson/serverless-url-shortener'
+      )
+      .replace(/INSERT_CREDIT/g, CREDIT || 'View this project on Github')
+  )
 
   console.log('Template build success')
 } catch (error) {
